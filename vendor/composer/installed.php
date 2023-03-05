d6d2756ffba76466ae7bf76de286ded993ab4a63
chetakogo <chudakova.lyuba.00@gmail.com>
2023-03-05 17:27:01 +0300

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'liubov/karavel1',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7c71e7a2839c5a35dd523b377f5abd417dd5047b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'liubov/karavel1' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7c71e7a2839c5a35dd523b377f5abd417dd5047b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
